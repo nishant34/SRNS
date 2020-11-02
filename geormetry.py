161a6505e5e660311eb47c69018c62d3a04f142e
nishant jain <njain@cs.iitr.ac.in>
2020-11-03 00:36:45 +0530

    Update geormetry.py

--- geormetry.py
+++ geormetry.py
@@ -1,5 +1,6 @@
 import numpy as  np
 import torch
+import torch.nn.functional as F
 import cv2
 from comon import *
 #def get_intrinsic(file_name,image_length):
@@ -13,15 +14,20 @@ def world_coordinate_generator(R,K,t,u,v,d):
     b = np.linalg.inv(K)
     ans = a*(np.matmul(b,vect)-t)
     return ans
+
+
+
 def get_normal_map_from_depth_map(d_im):
     h,w,d = d_im.shape
+    normals = torch.zeros(h,w,d)
     for i in range(1,w-1):
       for j in range(1,h-1):
         t = np.array([i,j-1,d_im[j-1,i,0]],dtype="float64")
         f = np.array([i-1,j,d_im[j,i-1,0]],dtype="float64")
         c = np.array([i,j,d_im[j,i,0]] , dtype = "float64")
         #d = np.cross(f-c,t-c)
-        d = torch.cross(f-c,t-c)
+        d = torch.cross(torch.tensor(f-c),torch.tensor(t-c))
+        #print("cross_passed")
         #n = d / np.sqrt((np.sum(d**2)))
         n = F.normalize(d,dim=-1)
         normals[j,i,:] = n
@@ -29,9 +35,37 @@ def get_normal_map_from_depth_map(d_im):
 
 def depth_map_to_cam_coordinates(x_im,y_im,z,intrinsics):
     fx,fy,cx,cy = get_intrinsic_params(intrinsics)
-    x_cam  = ((x_im-cx)*z)/fx
-    y_cam  =  ((y_im-cy)*z)/fy
+    #print(fx.shape)
+    #print(fy.shape)
+    #print(cx.shape)
+    #print(cy.shape)
+    #print(x_im.shape)
+    #print(y_im.shape)
+    #print(z.shape)
+    num_pixels = x_im.shape[1]
+    #x_cam = torch.zeros(x_im.shape[0],num_pixels)
+    #y_cam = torch.zeros(x_im.shape[0],num_pixels)
+    cx_1 = cx[:,None]
+    cy_1 = cy[:,None]
+    fx_1 = fx[:,None]
+    fy_1 = fy[:,None]
     
+
+    cx_1 = cx_1.expand(4,num_pixels)
+    cy_1 = cy_1.expand(4,num_pixels)
+    fx_1 = fx_1.expand(4,num_pixels)
+    fy_1 = fy_1.expand(4,num_pixels)
+    cx_1  = cx_1.to(device)
+    cy_1 =  cy_1.to(device)
+    fx_1  = cx_1.to(device)
+    fy_1 =  cy_1.to(device)
+    x_im = x_im.to(device)
+    y_im = y_im.to(device)
+    x_cam  = ((x_im-cx_1)*z)/fx_1
+    y_cam  =  ((y_im-cy_1)*z)/fy_1
+    x_cam  = x_cam.float()
+    y_cam =  y_cam.float()
+    #print("barrier_passed")
     return torch.stack((x_cam,y_cam,z),dim=-1)
     
 
@@ -82,9 +116,6 @@ def get_depths_from_world_coordinates(x,y,z,extrinsic_matrix):
 
     return depth
 
-
-
-    
 
 
     
